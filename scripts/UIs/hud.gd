1f13c119fd400737d8b802a611a8b4f06b4ecc25
Thomas Collins <collinskthomas@gmail.com>
2023-11-04 05:35:49 +0000

    Implement variious UIs
    - home screen
    - settings screen
    - main game screen

--- scripts/UIs/hud.gd
+++ scripts/UIs/hud.gd
@@ -2,55 +2,47 @@ extends Control
 
 # COINS
 func _on_player_coin_collected(coin_count):
-	$CoinCount.text = "Coins: " + str(coin_count)
+	$CoinCount.text = str(coin_count)
 
 # SHIELD
 func _on_player_shield_collected():
-	$ShieldSprite.stop()
-	$ShieldSprite.visible = true
-	$ShieldSprite/ProgressBar.visible = false
+	$Shield/ProgressBar.set_max(30)
+	$Shield/ProgressBar.value = 30 
 
 func _on_player_shield_up(up_time):
-	# enable and play shield animated sprite
-	$ShieldSprite.play()
-	$ShieldSprite.visible = true
-	$ShieldSprite/ProgressBar.visible = true
-	$ShieldSprite/ProgressBar.set_max(up_time * 10)
-	$ShieldSprite/ProgressBar.value = up_time * 10 
-	$ShieldSprite/ProgressTimer.set_wait_time(0.1)
-	$ShieldSprite/ProgressTimer.start()
+	$Shield/ProgressBar.set_max(up_time * 10)
+	$Shield/ProgressBar.value = up_time * 10 
+	$Shield/ProgressTimer.set_wait_time(0.1)
+	$Shield/ProgressTimer.start()
 
 func _on_player_shield_dropped():
-	$ShieldSprite.visible = false
+	$Shield/ProgressBar.value = 0
+	$Shield/ProgressTimer.stop()
 
 func _on_shield_progress_timer_timeout():
 	# increase the progress indicator
-	if $ShieldSprite/ProgressBar.value > 0:
-		$ShieldSprite/ProgressBar.value -= 1
+	if $Shield/ProgressBar.value > 0:
+		$Shield/ProgressBar.value -= 1
 
 # HOVER
 func _on_player_hover_collected():
-	$HoverSprite.stop()
-	$HoverSprite.visible = true
-	$HoverSprite/ProgressBar.visible = false
+	$Hover/ProgressBar.set_max(30)
+	$Hover/ProgressBar.value = 30 
 
 func _on_player_hover_on(on_time):
-	# enable and play hover animated sprite
-	$HoverSprite.play()
-	$HoverSprite.visible = true
-	$HoverSprite/ProgressBar.visible = true
-	$HoverSprite/ProgressBar.set_max(on_time * 10)
-	$HoverSprite/ProgressBar.value = on_time * 10 
-	$HoverSprite/ProgressTimer.set_wait_time(0.1)
-	$HoverSprite/ProgressTimer.start()
+	$Hover/ProgressBar.set_max(on_time * 10)
+	$Hover/ProgressBar.value = on_time * 10 
+	$Hover/ProgressTimer.set_wait_time(0.1)
+	$Hover/ProgressTimer.start()
 
 func _on_player_hover_dropped():
-	$HoverSprite.visible = false
+	$Hover/ProgressBar.value = 0
+	$Hover/ProgressTimer.stop()
 	
 func _on_hover_progress_timer_timeout():
 	# increase the progress indicator
-	if $HoverSprite/ProgressBar.value > 0:
-		$HoverSprite/ProgressBar.value -= 1
+	if $Hover/ProgressBar.value > 0:
+		$Hover/ProgressBar.value -= 1
 	
 # SCORE TIME
 # update the score time every second
@@ -61,7 +53,7 @@ func _on_player_score_one_second_elapsed(total_score_time):
 func format_time(total_time):
 	var minutes = int(total_time / 60)
 	var seconds = total_time % 60
-	return "Time: " + str(minutes).pad_zeros(2) + ":" + str(seconds).pad_zeros(2)
+	return str(minutes).pad_zeros(2) + ":" + str(seconds).pad_zeros(2)
 
 func _on_daily_challenge_task_updated(tasks):
 	print("DAILY CHALLENGE TASKS")
